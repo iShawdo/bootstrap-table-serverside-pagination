e6cf2c1e70232fc7e8267a9db676a7c7241148ce
Michael Joseph Nuñez Bobadilla <michael.nunez@arkanosoft.com>
2017-12-25 20:57:14 -0300

    Se agrega el proyecto al repositorio.

---
+++ BootstrapTableExample/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BootstrapTableExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public List<Row> data = new List<Row>();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetData(string order, int offset, int limit)
+        {
+            if (data.Count == 0)
+            {
+                for (int i = 1; i <= 100; i++)
+                {
+                    Row item = new Row()
+                    {
+                        Id = i,
+                        Name = "Item " + i,
+                        Price = "$" + i
+                    };
+                    data.Add(item);
+                }
+            }
+
+            JsonResult response = new JsonResult()
+            {
+                Data = new { total = data.Count, rows = data.Select(x => x).Skip(offset).Take(limit).ToList() },
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet
+            };
+            return response;
+        }
+
+        public class Row
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Price { get; set; }
+        }
+
+    }
+}
